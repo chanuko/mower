ca562344a5beb6b745150d2bfdadac6f527486e7
Gontoh <chanuko@gontoh.net>
2020-07-02 17:40:30 +0900

    MOD IDを追加

---
+++ src/main/java/net/gontoh/mower/Mower.java
@@ -0,0 +1,14 @@
+package net.gontoh.mower;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(Mower.MOD_ID)
+public class Mower {
+    public static final String MOD_ID = "mower";
+    public static final Logger LOGGER = LogManager.getLogger();
+    
+}
